0111a588a5eab4c15a682aeeb723f8e707cbb12c
A K <ameya.2000k@gmail.com>
2021-07-02 18:14:17 +0530

    Complete List Functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 
 function App() {
+  const[userList, addUserList] = useState([]);
+  const addUserHandler = (uName,uAge) =>{
+    addUserList((prev) => {
+      return [...prev,{name: uName, age: uAge, key: Math.random().toString()}]
+    })
+  }
   return (
     <div>
-    <AddUser/>
+    <AddUser onAddUser={addUserHandler}/>
+    <UserList users={userList}/>
     </div>
   );
 }
